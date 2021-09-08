1b13716fe1234974cf3d2d03aef42542536787ae
Julija Faltina <dashinicha@gmail.com>
2021-09-08 10:57:46 +0300

    Added fetch movies by id

--- src/services/moviesApiService.jsx
+++ src/services/moviesApiService.jsx
@@ -12,9 +12,15 @@ function fetchMoviesByName(movieName, pageNumber) {
       `${BASE_URL}/search/movie?api_key=${API_KEY}&page=${pageNumber}&language=en-US&include_adult=false&query=${movieName}`
     ).then(responseStatusHandling);
 }
-
+//список самых популярных фильмов на сегодня
 function fetchPopularMoviesByDay(pageNumber) {
     return fetch(
       `${BASE_URL}/trending/movie/day?api_key=${API_KEY}&page=${pageNumber}&language=en-US`
+    ).then(responseStatusHandling);
+}
+//поиск кинофильма по ключевому слову
+function fetchMoviesById(movieId) {
+    return fetch(
+        `${BASE_URL}/movie/${movieId}?api_key=${API_KEY}&language=en-US`
     ).then(responseStatusHandling);
 }
