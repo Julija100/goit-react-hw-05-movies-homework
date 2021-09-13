0d233114f92ee996c0ba9db23b823e5baf514ce3
Julija Faltina <dashinicha@gmail.com>
2021-09-13 10:37:05 +0300

    install @material-ui/core

--- src/components/MovieGallery/MovieGallery.jsx
+++ src/components/MovieGallery/MovieGallery.jsx
@@ -4,4 +4,20 @@ import PropTypes from 'prop-types';
 import { BASE_URL_IMG } from '../../services/moviesApi';
 
 
-export default function MoviesGallery ({ movies, url })
+export default function MoviesGallery({ movies, url }) {
+    const location = useLocation();
+
+    return (
+        <div>
+            {movies.map(({ id, poster_path, title }) => (
+                <Link
+                    to={{
+                        pathname: `${url}/${id}`,
+                        state: {from: location },
+                    }}
+                ></Link>
+            )
+)}
+        </div>
+    )
+}
